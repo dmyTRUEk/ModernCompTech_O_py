c6e6e8b088746668c935c180167b8ea4308bbe8f
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2023-10-13 19:49:20 +0300

    refactor(task7): impl `one_hot_at`, move `train` & `test` funcs to NN

--- task7_nn_to_dist_numbers/main.py
+++ task7_nn_to_dist_numbers/main.py
@@ -1,6 +1,6 @@
 # Neural Network that "sees" numbers.
 
-from random import uniform as random_float
+from random import shuffle, uniform as random_float
 from math import exp
 
 import numpy as np
@@ -8,25 +8,32 @@ import numpy as np
 from pipe import Pipe, map as map_
 
 
-EPOCHS: int = 100
+EPOCHS: int = 1_000
 
 LEARNING_RATE: float = 0.01
 
 
 def main():
-    x_train, y_train, x_test, y_test = load_datasets(["./Data_train.csv", "./Data_test.csv"])
+    x_train, y_train, x_test, y_test = load_datasets(0.7, ["./Data_train.csv", "./Data_test.csv"])
+    # x_train, y_train = load_dataset("./Data_train.csv")
+    # x_test, y_test = load_dataset("./Data_test.csv")
 
     nn = NeuralNetwork(64, 10)
     print("Training Neural Network...")
-    train_nn(nn, x_train, y_train)
-    print("Training finished.")
-    print()
-    print(nn)
-    print()
+    nn.train(x_train, y_train)
+    print("Training finished.\n")
+
+    print(nn, "\n")
+
     print("NeuralNetwork test:")
-    test_nn(nn, x_test, y_test)
+    confusion_matrix = nn.test(x_test, y_test)
+    print("Confusion matrix:")
+    print(confusion_matrix)
+    precision = np.sum(confusion_matrix.diagonal()) / np.sum(confusion_matrix)
+    print(f"Neural Network Precision: {precision}")
     print()
 
+    # return
     while (inp := input("Input input to try Neural Network: ")) != "":
         try:
             inp = inp.split(',') | map_(float) | list_
@@ -37,20 +44,6 @@ def main():
         except Exception as e:
             print(f"Error: {e}")
 
-
-def train_nn(nn: "NeuralNetwork", x: list[np.ndarray], y: list[int]):
-    for neuron_number, neuron in enumerate(nn.neurons):
-        neuron.train(x, y, neuron_number)
-
-
-def test_nn(nn: "NeuralNetwork", x: list[np.ndarray], y: list[int]):
-    confusion_matrix = np.zeros((10, 10), dtype=np.int32)
-    for i in range(len(x)):
-        y_prediction = nn.process_input(x[i])
-        y_expected = y[i]
-        confusion_matrix[y_expected, y_prediction] += 1
-    print("Confusion matrix:")
-    print(confusion_matrix)
 
 
 def activation_function(x: float) -> float:
@@ -84,9 +77,8 @@ class Neuron:
         assert len(x) == len(y)
         for epoch in range(EPOCHS):
             for train_i in range(len(x)):
-                # print(self)
                 xi = x[train_i]
-                y_expected = (y[train_i] | one_hot_)[neuron_number]
+                y_expected = y[train_i] | one_hot_at_(neuron_number)
                 y_actual = self.process_input(xi)
                 y_error = y_expected - y_actual
                 self.weights += LEARNING_RATE * y_error * xi
@@ -121,20 +113,42 @@ class NeuralNetwork:
     def process_input(self, input: np.ndarray) -> int:
         return self.process_raw_output(self.process_input_raw(input))
 
+    def train(self, x: list[np.ndarray], y: list[int]):
+        assert len(x) == len(y)
+        for neuron_number, neuron in enumerate(self.neurons):
+            neuron.train(x, y, neuron_number)
 
-def load_datasets(filenames: list[str]) -> tuple[list[np.ndarray], list[int], list[np.ndarray], list[int]]:
+    def test(self, x: list[np.ndarray], y: list[int]) -> np.ndarray:
+        assert len(x) == len(y)
+        confusion_matrix = np.zeros((10, 10), dtype=np.int32)
+        for i in range(len(x)):
+            y_prediction = self.process_input(x[i])
+            y_expected = y[i]
+            confusion_matrix[y_expected, y_prediction] += 1
+        return confusion_matrix
+
+
+def load_dataset(filename: str) -> tuple[list[np.ndarray], list[int]]:
+    x: list[np.ndarray] = []
+    y: list[int] = []
+    with open(filename) as file:
+        for line in file.readlines():
+            yi, xi = line.strip().split(',') | split_at_(1)
+            y.append(int(yi[0]))
+            x.append(np.array(xi | map_(float) | list_))
+    return x, y
+
+
+def load_datasets(split_k: float, filenames: list[str]) -> tuple[list[np.ndarray], list[int], list[np.ndarray], list[int]]:
     xy_all: list[tuple[np.ndarray, int]] = []
     for filename in filenames:
-        with open(filename) as file:
-            for line in file.readlines():
-                y, x = line.strip().split(',') | split_at_(1)
-                y = int(y[0])
-                x = np.array(x | map_(float) | list_)
-                xy_all.append((x, y))
-    xy_train, xy_test = xy_all | split_at_percentage_(0.7)
+        x, y = load_dataset(filename)
+        for xi, yi in zip(x, y):
+            xy_all.append((xi, yi))
+    shuffle(xy_all)
+    xy_train, xy_test = xy_all | split_at_percentage_(split_k)
     x_train, y_train = xy_train | unzip_
     x_test, y_test = xy_test | unzip_
-
     return x_train, y_train, x_test, y_test
 
 
@@ -143,6 +157,7 @@ def load_datasets(filenames: list[str]) -> tuple[list[np.ndarray], list[int], li
 
 list_ = Pipe(list)
 sum_ = Pipe(sum)
+# zip_ = Pipe(zip)
 
 def unzip(xy: list[tuple["X", "Y"]]) -> tuple[list["X"], list["Y"]]: # pyright: ignore[reportUndefinedVariable]
     x = [el for el, _ in xy]
@@ -170,18 +185,39 @@ def index_of_max(l: list["T"]) -> int: # pyright: ignore[reportUndefinedVariable
 index_of_max_ = Pipe(index_of_max)
 
 
+ONE_HOT_DELTA = 0.01
+
 def one_hot(n: int, n_max: int = 10) -> np.ndarray:
     '''
     if `n_max` == 2:
-      0 -> [0,0,1],
+      0 -> [1,0,0],
       1 -> [0,1,0],
-      2 -> [1,0,0].
+      2 -> [0,0,1].
+      but 0->0.01, 1->0.99
     '''
-    oh = np.zeros(n_max, dtype=np.float64)
-    oh[n] = 1.
+    # This code have to be consistent with `one_hot_at` function.
+    # If code here is changed, `one_hot_at` should also be changed to produce same output.
+    assert 0 <= n < n_max
+    oh = np.full(n_max, ONE_HOT_DELTA, dtype=np.float64)
+    oh[n] = 1. - ONE_HOT_DELTA
     return oh
 
 one_hot_ = Pipe(one_hot)
+
+
+def one_hot_at(n: int, index: int, n_max: int = 10) -> float:
+    # Naive, unoptimized, but consistent to `one_hot` function solution:
+    # return one_hot(n, n_max)[index]
+    # Must faster solution:
+    # This code have to be consistent with `one_hot` function.
+    assert 0 <= n < n_max
+    assert 0 <= index < n_max
+    if n == index:
+        return 1. - ONE_HOT_DELTA
+    else:
+        return ONE_HOT_DELTA
+
+one_hot_at_ = Pipe(one_hot_at)
 
 
 def join(l: list[str], s: str = '\n') -> str:
